f56a9f05d20d1d8de07a869d74f3bbdedb8c0ef5
Victory Odumeh <vodumeh@gmail.com>
2023-03-14 18:29:32 +0100

    saved

--- social/src/components/topbar/Topbar.jsx
+++ social/src/components/topbar/Topbar.jsx
@@ -3,9 +3,27 @@ import './topbar.css'
 const Topbar = () => {
     return ( 
         <div className='topbarContainer'>
-        <div className='topbarLeft'></div>
-        <div className='topbarCenter'></div>
-        <div className='topbarRight'></div>
+        <div className='topbarLeft'>
+            <span className='logo'><a href='/'><i class="fa-solid fa-ghost"></i></a></span>
+        </div>
+        <div className='topbarCenter'>
+            <div className='searchbar'>
+                <i class="fa-solid fa-magnifying-glass"></i>
+                <input placeholder="Search for stories or friends" className='searchInput' />
+            </div>
+        </div>
+        <div className='topbarRight'>
+            <div className='topbarLinks'>
+                <span className='topbarLink'>Homepage</span>
+                <span className='topbarLink'>Timeline</span>
+            </div>
+            <div className='topbarIcons'>
+               <div className='topbarIconItem'>
+                    <i class="fa-solid fa-user"></i>
+                    <span className='topbarIconBadge'>1</span>
+                </div> 
+            </div>
+        </div>
         </div>
      );
 }
